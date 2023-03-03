9006ae42ffee879067aa581f531b890ea2d6df64
Heather B <heatherbrokmeier@gmail.com>
2023-03-03 00:10:48 -0600

    Upgrade 11ty & add sass

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,25 @@
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("img");
+  let markdownIt = require('markdown-it')({
+    html: true,
+    breaks: true,
+    linkify: true,
+  });
+  
+  eleventyConfig.setLibrary('md', markdownIt.use(require('markdown-it-attrs')));
+
+  // Copy anything in assets
+  eleventyConfig.addPassthroughCopy('assets');
+
+  // Don't watch .gitignore
+  // git ignores compiled CSS, but lets 11ty recompile on changes to CSS file
+  eleventyConfig.setUseGitIgnore(false);
+
+  return {
+		dir: {
+			input: "content",
+			includes: "../_includes",
+			data: "../_data",
+			output: "_site",
+		},
+  }
 };
---
+++ _data/metadata.js
@@ -0,0 +1,10 @@
+module.exports = {
+	"title": "Travis Phillips, Musician",
+	"url": "https://travisphillipsmusician.com/",
+	"language": "en",
+	"description": "",
+	"author": {
+		"name": "Travis Phillips",
+		"email": "travisphillipsmusician@gmail.com"
+	}
+}
